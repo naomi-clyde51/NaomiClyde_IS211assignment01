de79ba4c2eee5d8d4fe0745364718feb96ae9d3f
Naomi Clyde <49344161+naomi-clyde51@users.noreply.github.com>
2021-09-15 21:44:49 -0400

    commit my changes to assignment 1 part 2

---
+++ assignment1_part2.py
@@ -0,0 +1,14 @@
+class Book:
+    def __init__(self, author, title):
+        self.author = author
+        self.title = title
+
+    def display(self):
+        print (f"{self.title}, written by {self.author}.")
+
+if __name__ == "__main__":
+    book1 = Book("John Steinbeck", "'Of Mice and Men'")
+    book2 = Book("Harper Lee", "'To Kill a Mockingbird'")
+
+    book1.display()
+    book2.display()
